a83e0396911a50f2876529365a88a474b1a93e5a
keside <wilsonezeala@yahoo.com>
2021-07-09 06:48:25 +0100

    photo info added

--- src/components/Photos/Photo/Photo.jsx
+++ src/components/Photos/Photo/Photo.jsx
@@ -4,10 +4,14 @@ import "./Photo.css"
 const  Photo = () => {
     return (
         <div className="photo">
-        <img src="https://images.unsplash.com/photo-1527203561188-dae1bc1a417f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNDIyNTJ8MHwxfHNlYXJjaHwxfHxwZW9wbGV8ZW58MHwxfHxvcmFuZ2V8MTYyNTc3NTIwOA&ixlib=rb-1.2.1&q=80&w=1080" alt="photo 1" />
-        <div className="photo__info">
-          <p>photo by constantine</p>
+            <div className="photo__info">
+            <div className="photo__info-img">
+                <img src="https://images.unsplash.com/profile-1599446683222-b1e5543a0b3dimage?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128" alt="" />
+            </div>
+          <p  className="photo__info-name">constantine</p>
         </div>
+        <img src="https://images.unsplash.com/photo-1527203561188-dae1bc1a417f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNDIyNTJ8MHwxfHNlYXJjaHwxfHxwZW9wbGV8ZW58MHwxfHxvcmFuZ2V8MTYyNTc3NTIwOA&ixlib=rb-1.2.1&q=80&w=1080" alt="photo 1" />
+
       </div>
     )
 }
